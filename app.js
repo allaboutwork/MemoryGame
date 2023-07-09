c5835366e89a4298b9cf95595da7c7fce69237ea
allaboutwork <afshanalamwork@gmail.com>
2023-07-09 14:00:58 +0530

    added checkformatch and flipcard function

--- app.js
+++ app.js
@@ -52,7 +52,14 @@ const cardArray = [
     }
 ]
 
+cardArray.sort(() => 0.5 - Math.random())
+
 const grid = document.querySelector('.grid')
+const resultDisplay = document.querySelector('#result')
+
+var cardsChosen = []
+var cardsChosenId = []
+var cardsWon = []
 
 // create game board
 function createBoard () {
@@ -61,8 +68,51 @@ function createBoard () {
         var card = document.createElement('img')
         card.setAttribute('src', 'images/blank.png')
         card.setAttribute('data-id', i)
-        // card.addEventListener('click', flipcard)
+        card.addEventListener('click', flipcard)
         grid.appendChild(card)
+    }
+}
+
+// check for matches
+function checkforMatch(){
+    var cards = document.querySelectorAll('img')
+    const optionOneId = cardsChosenId[0]
+    const optionTwoId = cardsChosenId[1]
+    
+    if(optionOneId == optionTwoId){
+        cards[optionOneId].setAttribute('src','images/blank.png')
+        cards[optionTwoId].setAttribute('src','images/blank.png')
+        alert('You clicked the same image')
+    } else if ( cardsChosen[0] === cardsChosen[1]){
+        alert('You found a match')
+        cards[optionOneId].setAttribute('src', 'images/white.png')
+        cards[optionTwoId].setAttribute('src', 'images/white.png')
+        cards[optionOneId].removeEventListener('click', flipcard)
+        cards[optionTwoId].removeEventListener('click', flipcard)
+        cardsWon.push(cardsChosen)
+    } else {
+        cards[optionOneId].setAttribute('src', 'images/blank.png')
+        cards[optionTwoId].setAttribute('src', 'images/blank.png')
+        alert('Sorry try again')
+    }
+
+    cardsChosen = []
+    cardsChosenId = []
+    resultDisplay.textContent = cardsWon.length
+    if(cardsWon.length === cardArray.length/2){
+        resultDisplay.textContent = 'You won!'
+    }
+}
+
+// flip your card
+function flipcard(){
+
+    var cardId = this.getAttribute('data-id')
+    cardsChosen.push(cardArray[cardId].name)
+    cardsChosenId.push(cardId)
+    this.setAttribute('src', cardArray[cardId].img)
+    if(cardsChosen.length === 2){
+        setTimeout(checkforMatch, 500)
     }
 }
 
